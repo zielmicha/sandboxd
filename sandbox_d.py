a7aefb792f39a6a1b6dbc1b373789805d9a5bef6
Michał Zieliński <michal@zielinscy.org.pl>
2015-05-29 23:26:55 +0200

    sandbox_d: add listening on TCP sockets

--- sandbox_d.py
+++ sandbox_d.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python2.7
+# Copyright (c) 2013-2014 Michal Zielinski <michal@zielinscy.org.pl>
+# Copyright (c) 2015 Husarion Sp. z o.o.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import SocketServer
 import socket
 import os
@@ -34,5 +55,12 @@ class Handler(SocketServer.StreamRequestHandler):
 
 
 if __name__ == '__main__':
-    serv = ThreadingUnixServer(sys.argv[1], Handler)
-    serv.serve_forever()
+    if len(sys.argv) == 2:
+        serv = ThreadingUnixServer(sys.argv[1], Handler)
+        serv.serve_forever()
+    else:
+        port = int(sys.argv[2])
+        SocketServer.ThreadingTCPServer.allow_reuse_address = True
+        server = SocketServer.ThreadingTCPServer((sys.argv[1], port), Handler)
+        server.allow_reuse_address = True
+        server.serve_forever()
